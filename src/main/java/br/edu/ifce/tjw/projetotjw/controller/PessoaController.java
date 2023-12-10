73c03c90402d71af9b1a4c4bd25cdcb3a0901885
Joao <finkl1@hotmail.com>
2023-12-10 16:54:00 -0300

    :sparkles: feat: Criação do cadastro de pessoas

--- src/main/java/br/edu/ifce/tjw/projetotjw/controller/PessoaController.java
+++ src/main/java/br/edu/ifce/tjw/projetotjw/controller/PessoaController.java
@@ -3,7 +3,10 @@ package br.edu.ifce.tjw.projetotjw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.edu.ifce.tjw.projetotjw.entitie.Pessoa;
 import br.edu.ifce.tjw.projetotjw.repository.PessoaRepository;
 
 @Controller
@@ -18,6 +21,17 @@ public class PessoaController {
 	@GetMapping("/")
 	public String pessoas(Model model) {
 		model.addAttribute("listaPessoas", pessoaRepo.findAll());
-		return "index";
+		return "home-page";
+	}
+	
+	@GetMapping("/add-pessoa")
+	public String adicionarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		return "add-pessoa-page";
+	}
+	
+	@PostMapping("/save-pessoa")
+	public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		pessoaRepo.save(pessoa);
+		return "redirect:/";
 	}
 }
